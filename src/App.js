694823833e1b2ab6e0930fa09820b8123b1c03d1
elise <115508722+cultelise@users.noreply.github.com>
2022-11-24 15:29:01 -0600

    donezo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
 import "./App.css";
 import { useState } from "react";
+import Button from "./components/Button";
+import Statistics from "./components/Statistics";
+import Anecdotes from "./components/Anecdotes";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allStats, setAll] = useState(0);
+
+  let someStats;
+  if (allStats === 0) {
+    someStats = "hide";
+  } else someStats = "show";
+
+  let noStats;
+  if (allStats === 0) {
+    noStats = "show";
+  } else noStats = "hide";
+
+  const incrementAll = () => {
+    setAll(allStats + 1);
+  };
+
+  const incrementGood = () => {
+    incrementAll();
+    setGood(good + 1);
+  };
+
+  const incrementNeutral = () => {
+    incrementAll();
+    setNeutral(neutral + 1);
+  };
+
+  const incrementBad = () => {
+    incrementAll();
+    setBad(bad + 1);
+  };
 
   return (
     <div>
-      code here
+      <h2>give feedback</h2>
+      <Button onClick={incrementGood} text="good" />
+      <Button onClick={incrementNeutral} text="neutral" />
+      <Button onClick={incrementBad} text="bad" />
+      <h2>statistics</h2>
+      <div className={noStats}>No feedback given</div>
+      <div className={someStats}>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          allStats={allStats}
+        />
+      </div>
+      <Anecdotes />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
