48fc5cb89820d6e9a0835316b9c4afcd9d13e1e0
elise <115508722+cultelise@users.noreply.github.com>
2022-11-24 12:44:27 -0600

    new project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      code here
+    </div>
   )
 }
 
-export default App
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-let counter = 1
-
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-refresh()
-counter += 1
-refresh()
-counter += 1
-refresh()
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />)
