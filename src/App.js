0e18f1aafbbf021de391568c8295330a278578e5
elise <115508722+cultelise@users.noreply.github.com>
2022-11-24 08:54:47 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+const App = (props) => {
+  const {counter} = props
+  return (
+    <div>{counter}</div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+let counter = 1
+
+const refresh = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <App counter={counter} />
+  )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
